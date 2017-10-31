b97fa9a5344f90a74f8339f6a84e5391459cf314
Kamleshsam <31609904+Kamleshsam@users.noreply.github.com>
2017-10-31 14:58:10 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-import math #including math module
+import math #including math module by me
 radius = input('radius is ',)
 area = pow(float(radius), 2) * math.pi
 print('Area of circle is  ', area)
